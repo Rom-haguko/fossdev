0edaa970ade69886ec1a9571669e597d868f1059
RomG <romagureev170@gmail.com>
2026-02-05 15:20:40 +0800

    test for sum and devid improved

--- test.py
+++ test.py
@@ -1,5 +1,27 @@
-def test_sum():
-    return 0
+from script import sum, devide
 
-def test_divide():
-    return 0
+def test_sum():
+    a = 1
+    b = 2
+    result = 3
+    assert sun(a, b) == result
+
+
+def test_devide():
+    a = 2
+    b = 4
+    result = 0.5
+    assert devide(a, b) == result
+
+def test_devide_zero():
+    a = 2
+    b = 0
+    try:
+	sum(a, b)
+	assert False
+    except: ValueError as e:
+	print("Test (zero-devision) passed")
+
+if __name__ == "__main__":
+    test_devide()
+    test_sum()
